e4a14f24d62a5453555e3767cb3247652b8421b1
Hanzel <hanzelrainbow@protonmail.com>
2020-09-05 13:14:34 -0700

    Update text

--- src/Components/Staking.js
+++ src/Components/Staking.js
@@ -19,13 +19,7 @@ function Staking() {
         <li>Each Candy Rocket has 3 levels.</li>
         <li>Candy Rocket level 3 has two assets, split 50/50 by value at creation.</li>
         <li>Each level distributes 1 CANDY/hour.</li>
-        <li>Pearl Rocket:
-          <ol>
-            <li>Pearl</li>
-            <li>Pearl/TRX</li>
-            <li>Pearl/TRX + Candy</li>
-          </ol>
-        </li>
+        <li>IMPORTANT: Actual rocket assets are subject to change. These are examples!</li>
         <li>USDJ Rocket:
           <ol>
             <li>USDJ</li>
@@ -33,11 +27,39 @@ function Staking() {
             <li>USDJ/TRX + Candy</li>
           </ol>
         </li>
+        <li>Pearl Rocket:
+          <ol>
+            <li>Pearl</li>
+            <li>Pearl/TRX</li>
+            <li>Pearl/TRX + Candy</li>
+          </ol>
+        </li>
+        <li>JST Rocket:
+          <ol>
+            <li>JST</li>
+            <li>JST/TRX</li>
+            <li>JST/TRX + Candy</li>
+          </ol>
+        </li>
+        <li>JFI Rocket:
+          <ol>
+            <li>JFI</li>
+            <li>JFI/TRX</li>
+            <li>JFI/TRX + Candy</li>
+          </ol>
+        </li>
         <li>SAL Rocket:
           <ol>
             <li>SAL</li>
             <li>SAL/TRX</li>
             <li>SAL/TRX + Candy</li>
+          </ol>
+        </li>
+        <li>DZI Rocket:
+          <ol>
+            <li>DZI</li>
+            <li>DZI/TRX</li>
+            <li>DZI/TRX + Candy</li>
           </ol>
         </li>
         <li>TAI Rocket:
